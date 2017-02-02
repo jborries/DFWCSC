1b42de51408088da0f0b975fb47a946a1900b259
Jesse Borries <jesse.borries@Gmail.com>
2017-02-01 22:55:18 -0600

    Second Commit

---
+++ client/assets/angular/app.js
@@ -0,0 +1,36 @@
+var app = angular.module('app', ['ngRoute']);
+
+//ROUTES
+app.config(function($routeProvider, $httpProvider){
+	$routeProvider
+		.when("/", {
+			templateUrl: "./partials/landing.html",
+		})
+		.when("/dashboard", {
+			templateUrl: "./partials/dashboard.html",
+			controller: "QuestionsController"
+		})
+		.when("/new_question", {
+			templateUrl: "./partials/newQuestion.html",
+			controller: "QuestionsController"
+		})
+		.when("/question/:id", {
+			templateUrl: "./partials/showQuestion.html",
+			controller: "QuestionsController"
+		})
+		.when("/question/:id/new_answer", {
+			templateUrl: "./partials/newAnswer.html",
+			controller: "QuestionsController"
+		})
+		.when("/marketing", {
+			templateUrl: "./partials/marketing.html"
+		})
+		.when("/developer", {
+			templateUrl: "./partials/developer.html"
+		})
+		.when("/loginreg", {
+			templateUrl: "./partials/loginreg.html"
+		})
+
+
+})
